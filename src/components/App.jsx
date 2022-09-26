878ca07cde36b286e8e6851a3be1d7c969ea0faa
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-09-26 20:12:53 +0300

    test commit_2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Counter } from './Feedback/Test';
+
 export const App = () => {
   return (
     <div
@@ -9,6 +11,8 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
       }}
-    ></div>
+    >
+      <Counter />
+    </div>
   );
 };
--- src/components/Feedback/Test.jsx
+++ src/components/Feedback/Test.jsx
@@ -1,1 +1,41 @@
+import React from 'react';
+import css from './Test.module.css';
+import { Controls } from './Test2';
 
+export class Counter extends React.Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+
+  state = {
+    value: this.props.initialValue,
+  };
+
+  handleIncrement = event => {
+    this.setState(prevState => {
+      return {
+        value: prevState.value + 1,
+      };
+    });
+  };
+
+  handleDecrement = event => {
+    this.setState(prevState => {
+      return {
+        value: prevState.value - 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className={css.Counter}>
+        <span className={css.Counter__value}>{this.state.value}</span>
+        <Controls
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
+      </div>
+    );
+  }
+}
